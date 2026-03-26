22a8ad8593c3cacfcddd533e46c726230c5b1c59
worthless01 <worthless0302@gmail.com>
2026-03-26 17:13:45 +0000

    grsghyrghyea

--- lec_1.py
+++ lec_1.py
@@ -3,79 +3,88 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-#Определяем переменную величину
+# Определяем переменную величину
 frames = 500
 seconds_in_year = 365 * 24 * 60 * 60
-years = 1
+years = 2
 t = np.linspace(0, years*seconds_in_year, frames)
 
 # Определяем функцию для системы диф. уравнений
 def move_func(s, t):
-    x, v_x, y, v_y = s
-    
-    dxdt = v_x
-    dv_xdt = - G * M * x / (x**2 + y**2)**1.5
-    dydt = v_y
-    dv_ydt = - G * M * y / (x**2 + y**2)**1.5
-    
-    return dxdt, dv_xdt, dydt, dv_ydt
+    (x1, v_x1, y1, v_y1,
+     x2, v_x2, y2, v_y2) = s
+
+    # dxdt = v_x
+    # dv_xdt = - G * M * x / (x**2 + y**2)**1.5
+    # dydt = v_y
+    # dv_ydt = - G * M * y / (x**2 + y**2)**1.5
+
+    dxdt1 = v_x1
+    dv_xdt1 = - G * M * x1 / (x1**2 + y1**2)**1.5
+    dydt1 = v_y1
+    dv_ydt1 = - G * M * y1 / (x1**2 + y1**2)**1.5
+
+    dxdt2 = v_x2
+    dv_xdt2 = - G * M * x2 / (x2**2 + y2**2)**1.5
+    dydt2 = v_y2
+    dv_ydt2 = - G * M * y2 / (x2**2 + y2**2)**1.5
+
+    return (dxdt1, dv_xdt1, dydt1, dv_ydt1,
+            dxdt2, dv_xdt2, dydt2, dv_ydt2)
 
 # Определяем начальные значения и параметры
 G = 6.67 * 10**(-11)
 M = 1.998 * 10**(30)
+ae = 200 * 10**9
+edge = 2.5 * ae
+e = 0.5
 
-x0 = 149 * 10**9
-v_x0 = 0
-y0 = 0
-v_y0 = 30000
+def Ve(r, a):
+    return np.sqrt((G * M) / a * (2 / (1 - e) - 1 ))
 
-s0 = (x0, v_x0, y0, v_y0)
+# x0 = 0.5 * ae
+# v_x0 = 0
+# y0 = 0
+# v_y0 = - Ve(0.5*ae, ae)
+
+x01 = 0.5 * ae
+v_x01 = 0
+y01 = 0
+v_y01 = - Ve(0.5*ae, ae)
+
+x02 = 0.5 * ae *  np.cos(np.deg2rad(30))
+v_x02 = - Ve(0.5*ae, ae) * np.sin(np.deg2rad(30))
+y02 = - 0.5 * ae *  np.sin(np.deg2rad(30))
+v_y02 = Ve(0.5*ae, ae) * np.cos(np.deg2rad(30))
+
+s0 = (x01, v_x01, y01, v_y01,
+      x02, v_x02, y02, v_y02)
 
 sol = odeint(move_func, s0, t)
 
-#Строим решение в виде графика и анимируем
+# Строим решение в виде графика и анимируем
 fig, ax = plt.subplots()
 
-ball, = plt.plot([], [], 'o', color='b')
-ball_line, = plt.plot([], [], '-', color='b')
+number_points = 2
+points = []
+points_lines = []
 
-plt.plot([0], [0], 'o', color='y', ms=20)
-plt.plot(sol[:3][0], sol[:3][2])
-plt.savefig('test.png')
+for i in range(number_points):
+    points.append(plt.plot([], [], 'o', color='r'))
+    points_lines.append(plt.plot([], [], '-', color='r'))
 
 def animate(i):
-    ball.set_data([sol[i][0]], [sol[i][2]])
-    ball_line.set_data(sol[:i][0], sol[:i][2])
+    for j in range(number_points):
+        # Исправление: передаем точки как списки с одним элементом
+        points[j][0].set_data([sol[i, 4 * j]], [sol[i, 4 * j + 2]])
+        points_lines[j][0].set_data(sol[:i, 4 * j], sol[:i, 4 * j + 2])
 
-# # Строим решение в виде графика и анимируем
-# fig, ax = plt.subplots()
+ani = FuncAnimation(fig, animate, frames=frames, interval=30)
 
-# number_points = 1
-# points = []
-# points_lines = []
+plt.axis('equal')
+ax.set_xlim(-edge, edge)
+ax.set_ylim(-edge, edge)
 
-# for i in range(number_points):
-#     points.append(plt.plot([], [], 'o', color='r'))
-#     points_lines.append(plt.plot([], [], '-', color='r'))
+plt.plot([0], [0], 'o', color='r', ms=20)
 
-# def animate(i):
-#     for j in range(number_points):
-#         points[j][0].set_data(sol[i, 4 * j], sol[i, 4 * j + 2])
-#         points_lines[j][0].set_data(sol[:i, 4 * j], sol[:i, 4 * j + 2])
-
-
-# ani = FuncAnimation(fig, animate, frames=frames, interval=30)
-
-# plt.axis('equal')
-# edge = 2 * x0
-# ax.set_xlim(-edge, edge)
-# ax.set_ylim(-edge, edge)
-
-# ani.save('galaxy.gif')
-
-
-
-
-
-
-
+ani.save('galaxy.gif', writer='pillow')
